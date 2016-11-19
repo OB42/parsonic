9b9c8a2186b2fd4c125c15bb6cad8f6b213b6da6
oabtm <oabtm@users.noreply.github.com>
2016-11-20 00:06:33 +0100

    Add timeout, refactoring

--- parsonic.js
+++ parsonic.js
@@ -1,46 +1,53 @@
 "use strict";
-const Nightmare = require("nightmare"),
-nightmare = Nightmare({ show: false, width: 0, height: 0 })
-const events = require("events")
-class BrowserEmitter extends events {}
-const browserEmitter = new BrowserEmitter()
-var load
-var toExecuteOnLoad = []
-browserEmitter.on("load", () => {
-    toExecuteOnLoad.filter(function(f){
-        f()
-    })    
-})
+const Nightmare = require("nightmare");
+const nightmare = Nightmare({
+	show: false,
+	width: 0,
+	height: 0,
+	executionTimeout: 5000
+});
+const events = require("events");
+class Browser_Emitter extends events {};
+const browser_emitter = new Browser_Emitter();
+var load;
+var onload = [];
+browser_emitter.on("load", () => {
+    onload.filter((f) => {f();});
+});
 nightmare.goto('about:blank')
 .then(() => {
-    load = function(html, args, browserCallback, nodeFunction){
-        nightmare.evaluate(function(html, args, browserCallbackSource){
+    load = (html, args, browser_callback, node_callback) => {
+        nightmare.evaluate((html, args, browser_callback_source) => {
+			var result;
             var window = null;
-            var document = new DOMParser().parseFromString(html, "text/html")
-            var results
-            try{
-                results = eval("(" + browserCallbackSource + ")(document, args)")
+			try
+			{
+            	var document = new DOMParser().parseFromString(html, "text/html");
+                result = eval("(" + browser_callback_source + ")(document, args)");
             }
-            catch(err){
-                results = {error: "Error from the browser: " + err.stack}
+            catch (err)
+			{
+                result = {error: "Error from the browser: " + err.stack};
             }
-            return results
-        }, html, args, browserCallback.toString())
-        .then(nodeFunction)
+            return (result);
+        }, html, args, browser_callback.toString())
+        .then((result) => {
+			node_callback(result.error || null, result.error ? null : result);
+		})
+        .catch((error) => node_callback(error));
+    };
+    browser_emitter.emit("load");
+});
+exports.load = (html, args, browser_callback, node_callback) => {
+    if (typeof load === "function")
+	{
+        load(html, args, browser_callback, node_callback);
     }
-    browserEmitter.emit("load")
-
-})
-exports.load = function(html, args, browserCallback, nodeFunction){
-    if(typeof load === "function"){
-        load(html, args, browserCallback, nodeFunction)
+    else
+	{
+        onload.push(() => load(html, args, browser_callback, node_callback));
     }
-    else{
-        toExecuteOnLoad.push(function(){load(html, args, browserCallback, nodeFunction)})
-    }
-}
-exports.close = function(){
-    require('child_process').exec('kill -9 ' + nightmare.proc.pid, function(){
-        console.log("nightmare was terminated")
-    })
-}
+};
+exports.close = () => {
+    nightmare.proc.kill('SIGINT');
+};
